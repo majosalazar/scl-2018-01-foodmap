b92bfa2886ad6aa2c04c51ce0d754149cb006533
mariajosesalazar <mariaj.salazarf@gmail.com>
2018-07-26 11:13:09 -0400

    estructura basica

---
+++ src/js/app.js
@@ -0,0 +1,79 @@
+/*
+//probando la capacidad del navegador de geolocalizacion
+function findMe() {
+  let output = document.getElementById('map');
+  if (navigator.geolocation) {
+    console.log('geolocalización activada');
+    
+  }else {
+    console.log('geolocalización no funcional');
+  }
+  function localizacion(posicion) {
+    let latitude = posicion.coords.latitude;
+    let longitude = posicion.coords.longitude;
+
+    output.innerHTML = `<p>${latitude}</p>`;
+
+
+  }
+}
+*/
+
+let map;
+let infowindow;
+
+ function initMap()
+ {
+ // Creamos un mapa con las coordenadas actuales
+   navigator.geolocation.getCurrentPosition(function(pos) {
+
+   lat = pos.coords.latitude;
+   lon = pos.coords.longitude;
+
+   let myLatlng = new google.maps.LatLng(lat, lon);
+
+   let mapOptions = {
+     center: myLatlng,
+     zoom: 14,
+     mapTypeId: google.maps.MapTypeId.MAP
+   };
+
+   map = new google.maps.Map(document.getElementById("mapa"),  mapOptions);
+
+   // Creamos el infowindow
+   infowindow = new google.maps.InfoWindow();
+
+   // Especificamos la localización, el radio y el tipo de lugares que queremos obtener
+   let request = {
+     location: myLatlng,
+     radius: 5000,
+     types: ['restaurant']
+   };
+
+   // Creamos el servicio PlaceService y enviamos la petición.
+   let service = new google.maps.places.PlacesService(map);
+
+   service.nearbySearch(request, function(results, status) {
+     if (status === google.maps.places.PlacesServiceStatus.OK) {
+       for (var i = 0; i < results.length; i++) {
+         crearMarcador(results[i]);
+       }
+     }
+   });
+ });
+}
+
+ function crearMarcador(place)
+ {
+   // Creamos un marcador
+   let marker = new google.maps.Marker({
+     map: map,
+     position: place.geometry.location
+   });
+
+ // Asignamos el evento click del marcador
+   google.maps.event.addListener(marker, 'click', function() {
+     infowindow.setContent(place.name);
+     infowindow.open(map, this);
+   });
+   }
